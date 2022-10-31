cb666ceb26da3839e6c3c0519e65fd507d362388
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2022-10-31 19:21:30 +0100

    Add new argument orientation to enable landscape mode

--- R/plot_colProfile.R
+++ R/plot_colProfile.R
@@ -17,6 +17,8 @@
 #'If the original sequence (no enhancement) is not wanted in the plot, this should be indicated by `-1`.
 #'For instance: '`cycles = c(-1,3)` shows only the 3rd enhancement cycle in the plot.
 #'
+#'@param orientation [character] (*with default*): sets the orientation of the plot (`portrait`, `landscape`)
+#'
 #'@param plot [logical] (*with default*): enables/disables plot output. If disables, the function
 #'returns a [list] with [matrix] objects with the colour enhanced profiles
 #'
@@ -35,6 +37,7 @@ plot_colProfile <- function(
     depth_top = NULL,
     depth_bottom = NULL,
     cycles = 5L,
+    orientation = "portrait",
     plot = TRUE,
     ...
 ){
@@ -89,78 +92,161 @@ plot_colProfile <- function(
   ## allow a few ... plot arguments
   plot_settings <- modifyList(
     x = list(
-      ylim = c(depth_bottom[length(depth_bottom)], depth_top[1]),
+      ylim = if (orientation != "landscape") {
+        c(depth_bottom[length(depth_bottom)], depth_top[1])
+
+        } else {
+         c(depth_top[1], depth_bottom[length(depth_bottom)])
+
+        },
       ylab = "Depth [m]",
       main = NULL,
       border_lwd = 2
     ), val = list(...), keep.null = TRUE)
 
-  ## open plot area
-  old.par <- graphics::par(
-    oma = c(0,0,0,0),
-    mar = if (is.null(plot_settings$main)) c(0.1,4,0.1,0.1) else c(0.1,4,1.1,0.1),
-    omi = c(0.05,0.05,0.05,0.05))
-  on.exit(graphics::par(old.par))
 
+  ## Portrait -------
   ## span plot area
-  plot(
-    x = NULL,
-    y = NULL ,
-    ylim = plot_settings$ylim,
-    xlim = c(0.1, length(plot_cycles)),
-    xlab = "",
-    ylab = plot_settings$ylab,
-    xaxt = "n",
-    bty = "n",
-    las = 2)
+  if (orientation[1] != "landscape") {
+    ## set area settings
+    old.par <- graphics::par(
+      oma = c(0,0,0,0),
+      mar = if (is.null(plot_settings$main)) c(0.1,4,0.1,0.1) else c(0.1,4,1.1,0.1),
+      omi = c(0.05,0.05,0.05,0.05))
+    on.exit(graphics::par(old.par))
 
-  if (!is.null(plot_settings$main))
-    graphics::title(plot_settings$main)
+    ## open plot area
+    plot(
+      x = NULL,
+      y = NULL ,
+      ylim = plot_settings$ylim,
+      xlim = c(0.1, length(plot_cycles)),
+      xlab = "",
+      ylab = plot_settings$ylab,
+      xaxt = "n",
+      bty = "n",
+      las = 2)
 
-  ## add a few more ticks
-  graphics::axis(
-    side = 2,
-    at = unique(floor(depth_top)),
-    labels = FALSE,
-    tck = -.018,
-    lwd.ticks = 0.5,
-    lwd = 0)
+    if (!is.null(plot_settings$main))
+      graphics::title(plot_settings$main)
 
-  ## add profiles
-  for (c in 1:length(plot_cycles)) {
-    graphics::text(
-      x = c - 1 + 0.1,
-      y = max(graphics::par()$usr[1], plot_settings$ylim[2]),
-      labels = if (plot_cycles[c] == 1) "Original" else paste0("Cycle: #", plot_cycles[c] - 1),
-      adj = c(0,-0.7),
-      cex = 0.6)
+    ## add a few more ticks
+    graphics::axis(
+      side = 2,
+      at = unique(floor(depth_top)),
+      labels = FALSE,
+      tck = -.018,
+      lwd.ticks = 0.5,
+      lwd = 0)
 
-    ## plot super rectangle
-    graphics::rect(
-      xright = c,
-      xleft = c - 1 + 0.1,
-      ybottom = depth_bottom[length(depth_bottom)],
-      ytop = depth_top[1],
-      lwd = plot_settings$border_lwd
-    )
+    ## add profiles
+    for (c in 1:length(plot_cycles)) {
+      graphics::text(
+        x = c - 1 + 0.1,
+        y = max(graphics::par()$usr[1], plot_settings$ylim[2]),
+        labels = if (plot_cycles[c] == 1) "Original" else paste0("Cycle: #", plot_cycles[c] - 1),
+        adj = c(0,-0.7),
+        cex = 0.6)
 
-    ## internal loop
-    for (i in 1:length(depth_top)) {
-      ## fill super rectangle
+      ## plot super rectangle
       graphics::rect(
-        xleft = c - 1 + 0.1,
-        ybottom = depth_bottom[i],
         xright = c,
-        ytop = depth_top[i],
-        density = NULL,
-        col = grDevices::rgb(
-          l[[plot_cycles[c]]][i, 1],
-          l[[plot_cycles[c]]][i, 2],
-          l[[plot_cycles[c]]][i, 3],
-          alpha = 1),
-        border = NA,
-        lwd = 0)
+        xleft = c - 1 + 0.1,
+        ybottom = depth_bottom[length(depth_bottom)],
+        ytop = depth_top[1],
+        lwd = plot_settings$border_lwd
+      )
+
+      ## internal loop
+      for (i in 1:length(depth_top)) {
+        ## fill super rectangle
+        graphics::rect(
+          xleft = c - 1 + 0.1,
+          ybottom = depth_bottom[i],
+          xright = c,
+          ytop = depth_top[i],
+          density = NULL,
+          col = grDevices::rgb(
+            l[[plot_cycles[c]]][i, 1],
+            l[[plot_cycles[c]]][i, 2],
+            l[[plot_cycles[c]]][i, 3],
+            alpha = 1),
+          border = NA,
+          lwd = 0)
+      }
     }
-  }
+  } else {
+    ## Landscape -------
+
+    ## set area settings
+    old.par <- graphics::par(
+      oma = c(0,0,0,0),
+      mar = if (is.null(plot_settings$main)) c(4,0.1,0.1,0.1) else c(4,0.1,1.1,0.1),
+      omi = c(0.05,0.05,0.05,0.05))
+    on.exit(graphics::par(old.par))
+
+    ## open plot area
+    plot(
+      x = NULL,
+      y = NULL ,
+      xlim = plot_settings$ylim,
+      ylim = c(0.1, length(plot_cycles)),
+      ylab = "",
+      xlab = plot_settings$ylab,
+      yaxt = "n",
+      bty = "n",
+      las = 1)
+
+    if (!is.null(plot_settings$main))
+      graphics::title(plot_settings$main)
+
+    ## add a few more ticks
+    graphics::axis(
+      side = 1,
+      at = unique(floor(depth_top)),
+      labels = FALSE,
+      tck = -.018,
+      lwd.ticks = 0.5,
+      lwd = 0)
+
+    ## add profiles
+    for (c in 1:length(plot_cycles)) {
+      graphics::text(
+        y = c - 1 + 0.1,
+        x = min(graphics::par()$usr[1], plot_settings$xlim[2]),
+        labels = if (plot_cycles[c] == 1) "Original" else paste0("Cycle: #", plot_cycles[c] - 1),
+        adj = c(0,2),
+        srt = 90,
+        cex = 0.6)
+
+      ## plot super rectangle
+      graphics::rect(
+        xright = depth_bottom[length(depth_bottom)],
+        xleft = depth_top[1],
+        ybottom = c,
+        ytop = c - 1 + 0.1,
+        lwd = plot_settings$border_lwd
+      )
+
+      ## internal loop
+      for (i in 1:length(depth_top)) {
+        ## fill super rectangle
+        graphics::rect(
+          xleft = depth_top[i],
+          ybottom = c - 1 + 0.1,
+          xright = depth_bottom[i],
+          ytop = c,
+
+          density = NULL,
+          col = grDevices::rgb(
+            l[[plot_cycles[c]]][i, 1],
+            l[[plot_cycles[c]]][i, 2],
+            l[[plot_cycles[c]]][i, 3],
+            alpha = 1),
+          border = NA,
+          lwd = 0)
+      }
+     }
+    }##end else
 
 }
